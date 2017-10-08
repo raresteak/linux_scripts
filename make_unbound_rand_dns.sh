2b1ed883639cc471c837eb78130f4d218841e270
raresteak <raresteak@users.noreply.github.com>
2017-10-08 14:24:32 -0500

    Update make_unbound_rand_dns.sh

--- make_unbound_rand_dns.sh
+++ make_unbound_rand_dns.sh
@@ -7,11 +7,11 @@ for i in {1..254}
 do
     FIRST=`head /dev/urandom | tr -dc a-z | head -c 7 ; echo ''`
     LAST=`head /dev/urandom | tr -dc 0-9 | head -c 7 ; echo ''`
-    echo "local-data: \"$FIRST-$LAST.test IN A 10.10.10.$i\""
-    echo "local-data-ptr: \"10.10.10.$i $FIRST-$LAST.test\""
+    echo "local-data: \"$FIRST-$LAST.example IN A 10.10.10.$i\""
+    echo "local-data-ptr: \"10.10.10.$i $FIRST-$LAST.example\""
     done
 
     #example
-    #local-data: "ohwtthy-4110451.test IN A 10.10.10.124"
-    #local-data-ptr: "10.10.10.124 ohwtthy-4110451.test"
+    #local-data: "ohwtthy-4110451.example IN A 10.10.10.124"
+    #local-data-ptr: "10.10.10.124 ohwtthy-4110451.example"
 

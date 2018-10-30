3f62720d57f79bbf4a60ac449dbf66fbcc48c661
raresteak <raresteak@users.noreply.github.com>
2018-10-30 10:27:49 -0500

    Update regenerateSshKeys.sh

--- regenerateSshKeys.sh
+++ regenerateSshKeys.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Generate new ssh2 host keys
+# Useful for downloaded or shared vm images.
 tar cvf /etc/ssh.tar /etc/ssh
 ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
 ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa

b110b279d867c87ef49cdcb804a35d68a77b4554
raresteak <raresteak@users.noreply.github.com>
2018-10-30 10:25:03 -0500

    Create regenerateSshKeys.sh

---
+++ regenerateSshKeys.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Generate new ssh2 host keys
+tar cvf /etc/ssh.tar /etc/ssh
+ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa -b 521
+ls -ltr /etc/ssh/ssh_host*
